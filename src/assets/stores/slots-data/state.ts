4ac67b68b09fb595f6aa2759a151e037507c4835
Alexander Zaytsev <sanyageologist@mail.ru>
2025-03-30 19:39:05 +0300

    small changes

--- src/assets/stores/slots-data/state.ts
+++ src/assets/stores/slots-data/state.ts
@@ -34,7 +34,7 @@ export const createRPSSlice: StateCreator<
   balance: 100,
   touched: 0,
   isLoading: false,
-  slotsVariants: 5, //Should be from 2 to 9
+  slotsVariants: 2, //Should be from 2 to 9
   slotsSizeRow: 3,
   slotsSizeCol: 5,
   firstNumbers: [[]] as number[][],
--- src/components/slot_inside/SlotInside.tsx
+++ src/components/slot_inside/SlotInside.tsx
@@ -18,11 +18,15 @@ export function SlotInside({ data, slotNumber, num }: ISlotLine) {
 
   const winingCells = SlotsState((state) => state.winingCells);
 
-  const checkIsGood: [boolean, number] = [false, -1];
-  const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
+  const checkIsGood: [boolean, number[]] = [false, [-1]];
+  const colorsArray = ['red', 'blue', 'green', 'yellow', 'purple'];
+
+  const timings: number[] = [];
+  const colors: string[] = [];
+  const scales: number[] = [];
 
   if (winingCells[0] !== undefined && winingCells[0].length > 1) {
-    checkIsGood[0] = winingCells
+    const winCheckVar = winingCells
       .map((elem) => {
         return elem.map((sub) => {
           return sub
@@ -32,21 +36,45 @@ export function SlotInside({ data, slotNumber, num }: ISlotLine) {
       })
       .map((el) => {
         return el.includes(true);
-      })
-      .includes(true);
-    if (checkIsGood[0])
-      checkIsGood[1] = winingCells
-        .map((elem) => {
-          return elem.map((sub) => {
-            return sub
-              .toString()
-              .includes([slotNumber[0], slotNumber[1]].toString());
-          });
+      });
+
+    checkIsGood[0] = winCheckVar.includes(true);
+
+    setTimeout(
+      () => {
+        const isWin = document.getElementsByClassName(
+          `classToFind_${slotNumber[0]}_${slotNumber[1]}`
+        );
+        if (checkIsGood[0]) isWin![0].ariaLabel = 'win';
+      },
+      rollDuration * 1000 +
+        (SlotsState.getState().slotsSizeRow +
+          SlotsState.getState().slotsSizeCol -
+          2) *
+          100 +
+        100
+    );
+
+    if (checkIsGood[0]) {
+      checkIsGood[1] = winCheckVar
+        .map((el, ind) => {
+          if (el === true) return ind;
         })
-        .map((el) => {
-          return el.includes(true);
-        })
-        .lastIndexOf(true);
+        .filter((el) => el !== undefined);
+
+      for (let i = 0; i < checkIsGood[1].length * 5; i++) {
+        timings.push(
+          (1 / checkIsGood[1].length) * Math.floor(i / 5) +
+            (i % 5) / (4 * checkIsGood[1].length)
+        );
+        if (i % 5 === 4) {
+          colors.push(
+            `5px solid ${colorsArray[checkIsGood[1][Math.floor(i / 5)]]}`
+          );
+          scales.push(...[1, 1.05, 1.07, 1.1, 1.15]);
+        } else colors.push('5px solid transparent');
+      }
+    }
   }
 
   const size = useRef<HTMLDivElement>(null);
@@ -93,39 +121,45 @@ export function SlotInside({ data, slotNumber, num }: ISlotLine) {
           times: [0, 1],
           delay: (slotNumber[0] + slotNumber[1]) / 10,
         }}
+        className={`classToFind_${slotNumber[0]}_${slotNumber[1]}`}
+        // aria-label={checkIsGood[0] ? 'win' : ''}
       >
         {data.map((item, index) => (
           <motion.div
             className={styles.slot}
             key={index}
             aria-label={index.toString()}
+            initial={{
+              outlineOffset: '-10px',
+              borderRadius: '50%',
+              outline: '5px solid transparent',
+            }}
             animate={
               index === 0 && checkIsGood[0]
                 ? {
-                    scale: [1, 1.05, 1, 1.1, 1.05],
-                    outline: [
-                      'none',
-                      'none',
-                      'none',
-                      'none',
-                      `5px solid ${colors[checkIsGood[1]]}`,
-                    ],
+                    scale: scales,
+                    outline: colors,
+
                     outlineOffset: '-10px',
                     borderRadius: '50%',
                   }
-                : {}
+                : {
+                    outlineOffset: '-10px',
+                    borderRadius: '50%',
+                    outline: '5px solid transparent',
+                  }
             }
             transition={{
-              repeat: 3,
-              duration: 0.5,
-              times: [0, 0.25, 0.5, 0.75, 1],
+              // repeat: 3,
+              duration: checkIsGood[1].length * 1.5,
+              times: timings,
               delay: rollDuration + (slotNumber[0] + slotNumber[1] + 2) / 10,
             }}
           >
             <img
               className={styles.image}
-              // src={getImgUrl(slotType, `${slotType}-${item}`)}
-              src={`./${slotType}-${item}.png`}
+              src={getImgUrl(slotType, `${slotType}-${item}`)}
+              // src={`./${slotType}-${item}.png`}
               alt={`${slotType}-${item}`}
             />
           </motion.div>
@@ -137,8 +171,8 @@ export function SlotInside({ data, slotNumber, num }: ISlotLine) {
       <div className={styles.slot}>
         <img
           className={styles.image}
-          // src={getImgUrl(slotType, `${slotType}-${slotsVariants}`)}
-          src={`./${slotType}-${slotsVariants}.png`}
+          src={getImgUrl(slotType, `${slotType}-${slotsVariants}`)}
+          // src={`./${slotType}-${slotsVariants}.png`}
           alt={`${slotType}-${slotsVariants}`}
         />
       </div>
